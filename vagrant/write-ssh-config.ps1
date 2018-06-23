0a79467674b92e1513ea58299d56cf14353c1b68
Daniel Oliver <danieloliversoftware@gmail.com>
2018-06-23 15:35:51 -0400

    ssh and vagrant file

--- vagrant/write-ssh-config.ps1
+++ vagrant/write-ssh-config.ps1
@@ -1,9 +1,11 @@
-# vagrant up
+# Get the Vagrant SSH with paths for Windows-Subsystem-Linux
 $fileText = vagrant ssh-config | ForEach-Object {$_ -replace 'C:/', '/mnt/c/'}
 
+# Save vagrant SSH to this path
 $vagrantFolder = "~/.vagrant_keys"
 bash -c "mkdir -p $vagrantFolder"
 bash -c "chmod 700 $vagrantFolder"
+# Each of the private key files
 $bashkeyFiles = $fileText | Select-String -Pattern "/mnt/c" -CaseSensitive
 foreach ($item in $bashkeyFiles) {
     $index = $bashkeyFiles.IndexOf($item)
@@ -17,5 +19,6 @@ $fileText | Out-File -FilePath "vagrant-ssh.cfg" -Encoding ASCII
 bash -c "mv ./vagrant-ssh.cfg $vagrantFolder/vagrant-ssh.cfg"
 bash -c "chmod 644 $vagrantFolder/vagrant-ssh.cfg"
 
+# Ansible likes absolute path instead of tilda for home path.
 $ansibleCfg = Get-Content "../ansible/ansible.cfg" | ForEach-Object { $_ -replace "(~)|(/home/[^/]*)", (bash -c "readlink -e ~") } 
 $ansibleCfg | Out-File "../ansible/ansible.cfg" -Encoding ASCII
